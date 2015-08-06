76b12621400240cbc1af5032098ed200ad7c36ae
ArturBK <artur.koefender@ilegra.com>
2015-08-06 10:36:15 -0300

    novo projeto

---
+++ src/main/java/artur/controller/Hello.java
@@ -0,0 +1,29 @@
+package artur.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+public class Hello {
+ 
+	
+    @RequestMapping(value = "/hello")
+    public String hello(
+		Model model,
+		@CookieValue(value = "abc", defaultValue = "") String abc,  HttpServletRequest request){
+		try {
+
+    		model.addAttribute("qwerty", "qwerty");
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return "hello/hello123";
+    }
+ 
+}
+
