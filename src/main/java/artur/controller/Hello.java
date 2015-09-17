e437138ec4577385f141b459a0db69149701929b
= <artur.koefender@ilegra.com>
2015-09-17 13:48:05 -0300

    ...

--- src/main/java/artur/controller/Hello.java
+++ src/main/java/artur/controller/Hello.java
@@ -10,6 +10,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import artur.resource.Greeting;
 
 @Controller
+@EnableCaching
 public class Hello {
 		
 	private static final String TEMPLATE = "Hello, %s!";
@@ -35,6 +40,14 @@ public class Hello {
 
         return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
     }
+    
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("greeting");
+    }
+    
+    
+    
 	@RequestMapping("/")
 	String home() {
 		return "ok";
