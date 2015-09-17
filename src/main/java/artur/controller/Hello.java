f2ffa7d75469c5daf1c2624d71b6be84f87c4831
= <artur.koefender@ilegra.com>
2015-09-17 13:49:41 -0300

    ...

--- src/main/java/artur/controller/Hello.java
+++ src/main/java/artur/controller/Hello.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -32,6 +33,7 @@ public class Hello {
 
     @RequestMapping("/greeting")
     @ResponseBody
+    @Cacheable("greeting")
     public HttpEntity<Greeting> greeting(
             @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 
